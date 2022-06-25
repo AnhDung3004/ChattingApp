183c24d1faf01a6a9d973a5505a255d89ee3b4b8
Trần Anh Dũng <Trananhdung3004@gmail.com>
2022-06-25 20:18:21 +0700

    Fixed password hiding

--- app/src/main/java/com/example/chattingapp/AboutOurAppActivity.java
+++ app/src/main/java/com/example/chattingapp/AboutOurAppActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 import com.example.chattingapp.Models.Users;
 import com.example.chattingapp.databinding.ActivityAboutOurAppBinding;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -22,6 +23,8 @@ public class AboutOurAppActivity extends AppCompatActivity {
 
     ActivityAboutOurAppBinding binding;
     FirebaseAuth auth;
+    FirebaseUser firebaseUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +35,7 @@ public class AboutOurAppActivity extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
 
-        binding.txtThanksName.setText("Thank you (" + auth.getCurrentUser().getDisplayName() + ")");
+        binding.txtThanksName.setText("Thank you (" + auth.getCurrentUser().getEmail() + ")");
 
         binding.iconBackArrow.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,21 +49,21 @@ public class AboutOurAppActivity extends AppCompatActivity {
         binding.txtPrivacyPolicy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(AboutOurAppActivity.this, "Our Privicy Policy is available on our website, let's take a look now", Toast.LENGTH_SHORT).show();
+                Toast.makeText(AboutOurAppActivity.this, "Our Privacy Policy is available on our website, let's take a look now.", Toast.LENGTH_SHORT).show();
             }
         });
 
         binding.txtAboutUs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(AboutOurAppActivity.this, "Our Information is available on our website, let's take a look now", Toast.LENGTH_SHORT).show();
+                Toast.makeText(AboutOurAppActivity.this, "Our Information is available on our website, let's take a look now.", Toast.LENGTH_SHORT).show();
             }
         });
 
         binding.txtNotification.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(AboutOurAppActivity.this, "Don't miss our news, let's check notification on our website now", Toast.LENGTH_SHORT).show();
+                Toast.makeText(AboutOurAppActivity.this, "Don't miss our news, please turn on notification setting!", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -74,7 +77,7 @@ public class AboutOurAppActivity extends AppCompatActivity {
         binding.txtHelp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(AboutOurAppActivity.this, "If you need help, visit our website to read more", Toast.LENGTH_SHORT).show();
+                Toast.makeText(AboutOurAppActivity.this, "If you need help, visit our website to read more.", Toast.LENGTH_SHORT).show();
             }
         });
     }
